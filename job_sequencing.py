d80d7fd1a1c229cc3d3de317002e3ca693464fd6
Ranju kumari <kumariranju1606@gmail.com>
2022-12-16 06:26:21 +0530

    python_program

---
+++ job_sequencing.py
@@ -0,0 +1,21 @@
+# Job class with 3 required properties
+@dataclass
+class Job:
+    job_name: str
+    profit: int
+    deadline: int
+
+
+if _name_ == '_main_':
+    # taking input from users
+    job_names = input("Enter Job names: ").split(" ")
+    profits = list(map(int, input("Enter Profits: ").split(" ")))
+    deadlines = list(map(int, input("Enter Deadlines: ").split(" ")))
+
+    # Storing all jobs in an list
+    jobs = [Job(name, profit, deadline)
+            for (name, profit, deadline) in zip(job_names, profits, deadlines)]
+
+    print("\nAll Jobs are:")
+    for job in jobs:
+        print(job)
